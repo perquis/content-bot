cbb1e9013930d97645d63f23f1eada5a3c794556
perquis <thetahirpl@gmail.com>
2024-09-06 12:31:47 +0200

    :game_die: refactor(main.py): Created an utils directory to create small functions.

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-import os
-import shutil
-from uuid import uuid4
-
 import click
-import requests
-from bs4 import BeautifulSoup
 from html2text import HTML2Text
-from md2pdf.core import md2pdf
+
+from utils import create_filename, create_pdf, use_scraped_page
 
 
 @click.command()
 @click.option('--url', prompt='Medium URL', help='The URL must be from the medium.com domain.')
 def main(url = str):
     html2text = HTML2Text()
-    filename = str(uuid4()) + ".pdf"
-    raw_html = ""
+    scraped_page = use_scraped_page(url)
 
-    try:
-        raw_html = requests.get(url, timeout=30).text
-    except requests.exceptions.RequestException as e:
-        click.echo(click.style(f"An error occurred: {e}", fg='red'))
-        return
-
-    scraped_page = BeautifulSoup(raw_html, 'html.parser')
     article = scraped_page.find('article')
 
     title = article.find('h1').get_text()
+    filename = create_filename(title)
 
     title = html2text.handle(title)
     text = html2text.handle(str(article))
@@ -33,14 +21,7 @@ def main(url = str):
     text = text.split("Share")[1]
     markdown_content = f"# {title}\n\n{text}"
 
-    filename = title.strip().replace("/", "-").replace(" ", "-").replace(":", "-") + "." + filename
-
-    if not os.path.isdir("docs"):
-        os.mkdir("docs")
-    
-    md2pdf(filename, markdown_content, css_file_path="./styles/markdown.css")
-    shutil.move(filename, f"docs/{filename}")
-
+    create_pdf(filename, markdown_content)
     click.echo("Your article has been saved as a PDF.")
 
 
---
+++ utils/__init__.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from uuid import uuid4
+
+import click
+import requests
+from bs4 import BeautifulSoup
+from md2pdf.core import md2pdf
+
+
+def create_filename(title: str) -> str:
+    converted_title = title.strip().replace("/", "-").replace(" ", "-").replace(":", "-")
+    hashed_filename = str(uuid4())
+    return converted_title + "." + hashed_filename + ".pdf"
+
+
+def create_pdf(filename: str, markdown_content: str) -> None:
+    if not os.path.isdir("docs"):
+        os.mkdir("docs")
+    
+    md2pdf(filename, markdown_content, css_file_path="./styles/markdown.css")
+    shutil.move(filename, f"docs/{filename}")
+
+
+def use_scraped_page(url: str) -> BeautifulSoup:
+    raw_html = ""
+
+    try:
+        raw_html = requests.get(url, timeout=30).text
+    except requests.exceptions.RequestException as e:
+        click.echo(click.style(f"An error occurred: {e}", fg='red'))
+        return
+
+    return BeautifulSoup(raw_html, 'html.parser')
